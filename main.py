3e98ac4f92598081572ce54e4ccfbe5567fd47a8
Sudikshya <sudikshyakafle2058@gmail.com>
2024-03-08 22:51:45 +0545

    machine learning part

--- main.py
+++ main.py
@@ -6,14 +6,15 @@ from flask_jwt_extended import create_access_token,get_jwt,get_jwt_identity
 from flask_jwt_extended import JWTManager
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+
 # from flask_jwt_extended import jwt_required, get_jwt, get_jwt_identity, decode_token, revoke_token
 
 
 app=Flask(__name__)
 
 
+CORS(app, origins=["*"], methods=["GET", "POST", "PUT", "DELETE"], allow_headers=["Content-Type", "Authorization"])
 
-CORS(app, origins=["*"] )
 app.config['JWT_SECRET_KEY'] = '123456'
 app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://root:@localhost/loanprediction"
 app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(hours=1)
@@ -55,6 +56,7 @@ def login():
         "password": password 
     })
 @app.route("/signin", methods=["POST"])
+@cross_origin()
 def signin():
     if request.method == "POST":
         data = request.get_json()  # Retrieve JSON data from the request
@@ -106,5 +108,91 @@ def register():
         return jsonify({
              
             "message": "User registered successfully"}), 201
+
+
+@app.route('/api/send-data', methods=['POST', 'OPTIONS'])
+@cross_origin()
+def send_data():
+
+  data = request.json['data']
+  # if __name__ == '__main__':
+    # app.run(port=5000)
+  import pickle
+#Importing the necessary modules
+  import numpy as np
+  import pandas as pd
+  from Algorithm.LogisticRegression import Logistic_Regression
+
+
+
+  #Reading the test and train dataset
+  train_data = pd.read_csv(r'A:\Flask\datasets\testdata.csv')
+  test_data = pd.read_csv(r'A:\Flask\datasets\traindata.csv')
+
+  #Splitting the data into features and outputs
+  xtrain, ytrain = train_data.drop(columns = 'Loan_Status', axis = 1), train_data['Loan_Status']
+  xtest, ytest = test_data.drop(columns = 'Loan_Status', axis =1), test_data['Loan_Status']
+
+  #Converting the csv files into numpy ndarrays for Logistic_Regression module
+  xtrain = np.array(xtrain)
+  ytrain = np.array(ytrain)
+
+  xtest= np.array(xtest)
+  ytest =  np.array(ytest)
+
+  model = Logistic_Regression(lr = 0.01, n_iters = 50_000)
+  model.fit(xtrain, ytrain)
+  class_pred = model.predict(xtest)
+  # print('\n')
+  print(model.ConfusionMatrix(ytest, class_pred))
+  # print('\n')
+  print(f'Accuracy = {model.Accuracy(ytest, class_pred)}')
+
+  #data validation
+  if int(data['loanAmount']) > 10000000:
+    return {"Remarks":'Loan Amount should be in range Rs.10,000 - Rs.1,00,00,000'}
+
+
+  gender = 0 if data['Gender'] == "Female" else 1
+  married = 0 if data['married'] == "No" else 1 
+  dependent = int(data['dependents'].split(" ")[0]) 
+  education= 0 if data['education'] == "Not Graduate" else 1 
+  selfemployed= 0 if data['selfEmployed'] == "No" else 1
+  creditHistory = int(data['creditHistory'])
+  if data['Area']=='Urban':
+    propertyArea=2
+  elif data['Area']=='semiUrban':
+    propertyArea=1
+  else:
+    propertyArea=0
+  applicationIncomelog=np.log(int(data['applicantIncome']))
+  loanamountlog=np.log(int(data['loanAmount']))
+  loanamounttermlog=np.log(int(data['loanAmountTerm']))
+  totalincomelog= np.log(int(data['totalIncome']))
+
+
+
+
+
+
+  # Process the data and return a response
+  data_list = [gender,married,dependent,education,selfemployed,creditHistory, propertyArea,applicationIncomelog,loanamountlog, loanamounttermlog,totalincomelog] 
+  data_numpy = np.array(data_list)
+#   data_numpy=np.reshape (1,11)
+
+
+  print("-------------Data---------")
+  print(data_numpy)
+  print("---------------------prediction-------------------")
+  y_pred = model.predict_for_one(data_numpy)
+  print(y_pred)
+  if y_pred[0] == 1:
+    y_predictions = "Loan is acceptable!"
+  else:
+    y_predictions = "Loan is not acceptable"
+  print(y_predictions)
+  return {"Remarks":y_predictions}
+  
+
 if __name__=="__main__":
     app.run(debug=True)
